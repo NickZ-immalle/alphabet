19486bc84dd340e8ffaa1204a99b0ad49389bf37
Nick Zegels <nick.zegels@immalle.eu>
2017-12-15 15:50:42 +0100

    alphabet letter list in caps

--- Oef/Program.cs
+++ Oef/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Oef
 {
@@ -10,6 +9,35 @@ namespace Oef
     {
         static void Main(string[] args)
         {
+            List<string> alpha = new List<string>{ " A" , " B " , " C " };
+            alpha.Add(" D ");
+            alpha.Add(" E ");
+            alpha.Add(" F ");
+            alpha.Add(" G ");
+            alpha.Add(" H ");
+            alpha.Add(" I ");
+            alpha.Add(" J ");
+            alpha.Add(" K ");
+            alpha.Add(" L ");
+            alpha.Add(" M ");
+            alpha.Add(" N ");
+            alpha.Add(" O ");
+            alpha.Add(" P ");
+            alpha.Add(" Q ");
+            alpha.Add(" R ");
+            alpha.Add(" S ");
+            alpha.Add(" T ");
+            alpha.Add(" U ");
+            alpha.Add(" V ");
+            alpha.Add(" W ");
+            alpha.Add(" X ");
+            alpha.Add(" Y ");
+            alpha.Add(" Z ");
+
+            foreach (string letter in alpha)
+            {
+             Console.Write(letter);
+            }
         }
     }
 }
